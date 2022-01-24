9725cb89aeae28b3ee801b128026ac8495ef62de
CrisL1990 <lucchetta.cris@gmail.com>
2022-01-24 21:44:50 +0100

    Iniziato parte grafica

--- main.js
+++ main.js
@@ -1,66 +1,74 @@
 
-//Imposta prezzo per ogni km percorso
-const priceForKm = 0.21;
+const sconto20 = 20;
+const sconto40 = 40;
 
-//Imposta valore degli sconti
-const discount20 = 20;
-const discount40 = 40;
+const prezzoKm = 0.21;
 
-
-
-//Contengono il totale dello sconto applicato e il totale al netto di eventuali sconti
-let total = 0;
 
 let offerta = "";
-let carrozza = Math.floor(Math.random() * 5) + 1;
-let codiceCP = Math.floor(Math.random() * 10000) + 1;
+totale = 0
 
 
-let generate = document.getElementById("generate");
-let annulla = document.getElementById("annulla");
-let nomePasseggero = document.getElementById("nomePasseggero");
+let passeggero = document.getElementById("passeggero");
 let tipoOfferta = document.getElementById("tipoOfferta");
 let numCarrozza = document.getElementById("numCarrozza");
-let cpCode = document.getElementById("cpCode");
-let costo = document.getElementById("costo");
-let displayTicket = document.getElementsById("ticket")
+let codiceCP = document.getElementById("codiceCP");
+let costoBiglietto = document.getElementById("costoBiglietto");
 
-generate.addEventListener('click',
+const genera = document.getElementById("genera");
+const annulla = document.getElementById("annulla");
+let ticket = document.getElementById("biglietto");
+let counter = 0;
+
+genera.addEventListener('click',
+
     function(){
-
-        //Prezzo totale del ticket
-        let ticketPrice = km * priceForKm;
-
-        const name = document.getElementById("name").value;
-        const km =  document.getElementById("km").value;
-        const discountList = document.getElementById("discountList").value;
         
-        if (discountList == "Minorenne"){
-            offerta = "Sconto Minorenni"
-            total = ticketPrice * discount20 / 100;
-        }
+        if(counter == 0){
+            ticket.classList.add("active")  
 
-        else if(discountList == "Over 65"){
-            offerta = "Sconto Over 65"
-            total = ticketPrice * discount40 / 100;
-        }
+            const nome = document.getElementById("nome").value;
+            const km = parseInt(document.getElementById("km").value);
+            const eta = document.getElementById("eta");
+                
+            const carrozza = Math.floor(Math.random() * 5) + 1;
+            const cpCode = Math.floor(Math.random() * 100000) + 1;
+            let prezzoIntero = km * prezzoKm;
 
-        else if(discountList=="Adult"){
-            offerta = "Biglietto Standard"
-            total = ticketPrice
-        }
+            if(eta.value == "Minorenne"){
+                offerta = "Sconto minorenni";
+                totale = prezzoIntero * sconto20 / 100;
+            }
 
-       
-        nomePasseggero.append(name);
-        tipoOfferta.append(offerta);
-        numCarrozza.append(carrozza);
-        cpCode.append(codiceCP);
-        costo.append(total);
-        
+            else if(eta.value == "over65"){
+                offerta = "Sconto Over 65"
+                totale = prezzoIntero * sconto40 / 100;
+            }
+
+            else if(eta.value == "Adulto"){
+                offerta = "Tariffa adulto"
+                totale = prezzoIntero;
+            }
+
+            passeggero.append(nome);
+            tipoOfferta.append(offerta);
+            numCarrozza.append(carrozza);
+            codiceCP.append(cpCode);
+            costoBiglietto.append(totale.toFixed(2)); 
+            counter += 1;
+            }
+            
+          
+    }  
+);
+
+annulla.addEventListener('click',
+    function(){
+        counter = 0;
+        passeggero.remove(nome);
+        ticket.classList.remove("active");
 
     }
-
-    
 )
 
 
